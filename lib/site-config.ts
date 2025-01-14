cf7cee0da9440d34c886efa08e68e16fc78fc940
JonathanKnegtel <26600327-JonathanKnegtel@users.noreply.replit.com>
2025-01-14 09:04:50 +0000

    Update site config: remove extra tag from default filter.

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -10,7 +10,6 @@ export const siteConfig = {
   filterByProductIds: [], // Used to filter which blockchains to show in the full list.
   blockchainIds: [], // Filter default options. Filters the options of (productSupports, productDeployedOnFilter and assetDeployedOnFilter)
   blockchainProductTypeIds: [15, 16, 17], // Filter default options. used by productDeployedOnFilter.
-  tags: [id1736843678-c5ql2lWRR_Wgk-IlYthSKA
-], // Filter default options. used by tagsFilter.
+  tags: ["id1736843678-c5ql2lWRR_Wgk-IlYthSKA"], // Filter default options. used by tagsFilter.
   displayQueries: true
 };
