9367ceff64a0e5dded4533cff2080a5018a3bbc5
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2025-01-14 08:57:57 +0000

    Update

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,15 +1,16 @@
 export const siteConfig = {
-  pageTitle: 'The Grid | Data Explorer',
+  pageTitle: 'ElizaOS Demo Day | Data Explorer',
   verifiedTagId: 7,
   logoSrc: {
-    dark: '/thegrid-logo-white.svg',
-    light: '/thegrid-logo.svg'
+    dark: '/eliza.svg',
+    light: '/eliza.svg'
   },
   pageDescription:
     'Powering Discoverability of Assets, Products, and Services in Web3.',
   filterByProductIds: [], // Used to filter which blockchains to show in the full list.
   blockchainIds: [], // Filter default options. Filters the options of (productSupports, productDeployedOnFilter and assetDeployedOnFilter)
   blockchainProductTypeIds: [15, 16, 17], // Filter default options. used by productDeployedOnFilter.
-  tags: [], // Filter default options. used by tagsFilter.
+  tags: [id1736843678-c5ql2lWRR_Wgk-IlYthSKA
+], // Filter default options. used by tagsFilter.
   displayQueries: true
 };
